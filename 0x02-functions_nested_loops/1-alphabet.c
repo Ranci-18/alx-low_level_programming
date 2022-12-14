a5145c233ca2defcad6a446eacfbd1ce22428943
Frank <wanyoike39@gmail.com>
2022-12-13 23:18:01 -0800

    function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
 /**
  *maint - entry point
  *
- *
+ *print_alphabet - prints the alphabet
  *
  *Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
-/*function prints the alphabet*/
+
 {
 	char i = 'a';
 
