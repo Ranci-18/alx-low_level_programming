6d1c3a06f9ce8e7e7c714648f8812acb90c95384
Frank <wanyoike39@gmail.com>
2022-12-15 02:42:01 -0800

    Used ASCII values and it worked

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,9 @@ void print_most_numbers(void)
 {
 	int k;
 
-	for (k = 0; k <= 9; k++)
+	for (k = 48; k <= 58; k++)
 		{
-		if (k != 2 && k != 4)
+		if (k != 50 && k != 52)
 			{
 			_putchar(k);
 			}
