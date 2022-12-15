db572921e703be3c563e9a6a742545e5561f425f
Frank <wanyoike39@gmail.com>
2022-12-15 04:03:02 -0800

    last putchar moved to for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,17 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 		{
 		int j;
-
 		for (j = 0; j <= 14; j++)
 			{
-			_putchar(j);
+
+			if (j >= 10)
+				{
+				_putchar(j / 10 + '0');
+				}
+			_putchar(j % 10 + '0');
+				
 			}
 		_putchar('\n');
 		}
-	_putchar('\n');
 
 }
