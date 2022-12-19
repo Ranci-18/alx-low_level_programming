853403c3e653393b7b0750d58b3613d177d25435
Frank <wanyoike39@gmail.com>
2022-12-19 11:20:19 -0800

    print any other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  *puts2 - prints every other character in a string
@@ -10,9 +11,11 @@
 void puts2(char *str)
 {
 	int k;
+	int length;
 
+	length = strlen(str);
 	k = 0;
-	while (str[k] < '\0')
+	while (k < length)
 	{
 		putchar(str[k]);
 		k = k + 2;
