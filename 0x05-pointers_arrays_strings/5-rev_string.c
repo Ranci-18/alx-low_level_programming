d154d74315b3e689268cfae6e92e409b38e7d6b3
Frank <wanyoike39@gmail.com>
2022-12-19 10:25:52 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,17 @@
 void rev_string(char *s)
 {
 	int i;
+	int halfway;
+       
+	halfway = strlen(s) / 2;
 
-	for (i = strlen(s); i >= 0; i--)
-		putchar(s[i]);
+	for (i = 0; i < halfway; i++)
+	{
+		char c;
+		
+		c = s[i];
+		s[i] = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = c;
+
+	}
 }
