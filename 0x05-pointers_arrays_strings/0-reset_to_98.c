51fa2bd30e83076d010140cb901b0a9eb173bcc9
Frank <wanyoike39@gmail.com>
2022-12-19 06:17:54 -0800

    Pointer changes value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *rest_to_98 - resets value of int to 98
+ *reset_to_98 - resets value of int to 98
  *
- *
+ *@n: is pointer variable
  *
  *Return: nothing
  */
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
