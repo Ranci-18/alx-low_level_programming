b6674058f7d2fe6f6bf56bc9e324415c8b17d5de
Frank <wanyoike39@gmail.com>
2022-12-19 16:03:25 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int c;
 
-	while (c >= 0)
+	for (c = 0; src[c] != '\0'; c++)
 	{
 		dest[c] = src[c];
 		if (src[c] == '\0')
 		{
 			break;
 		}
-		c++;
 	}
 	return (dest);
 }
