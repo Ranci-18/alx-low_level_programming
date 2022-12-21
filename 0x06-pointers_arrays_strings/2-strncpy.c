49d65a25b10170c76152faa1333f89d26920349b
Frank <wanyoike39@gmail.com>
2022-12-21 01:07:43 -0800

    copy string upto n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ **_strncpy - function copies src string to dest string up to nth character
+ *
+ *@dest: destination string
+ @src: source string
+ @n: copy limit
+ *
+ *
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	for (; x < n; x++)
+	dest[x] = '\0';
+	return (dest);
+}
