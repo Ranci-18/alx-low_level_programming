726756430c82b70b8055ede412b00f19df173f29
Frank <wanyoike39@gmail.com>
2022-12-22 21:25:51 +0300

    advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ **rot13 - rotates characters 13 times
+ *
+ *
+ *Return: s
+ */
+char *rot13(char *s)
+{
+	int i; 
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			s[i] = ((s[i] - 'a' + 13) % 26) + 'a';
+			s[i] = ((s[i] - 'A' + 13) % 26) + 'A';
+		}
+	}
+	return (s);
+}
