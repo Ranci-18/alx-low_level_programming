750c2ac1229d4dbfa6290aa58ee17aab84ea08b4
Frank <wanyoike39@gmail.com>
2023-01-03 09:57:16 +0300

    even more pointers, arrays, and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *_memset- sets first n bytes of s with 'b'
+ *
+ *@s: source string
+ *@b: character to set
+ *@n: bytes to set
+ *
+ *Return: set string s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+		s[i] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(int c);
+char *_memset(char *s, char b, unsigned int n);
