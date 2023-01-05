5b7f05bc51257e16c5f8914d79a44e049d9ed14e
Frank <wanyoike39@gmail.com>
2023-01-05 14:55:25 +0300

    function recursion projects

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *_pow_recursion - returns the power of x
+ *@x: base
+ *@y: power
+ *Return: power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
