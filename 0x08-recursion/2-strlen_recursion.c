e9c32c3822dc903fbd78d5cef75c16036d0df8a7
Frank <wanyoike39@gmail.com>
2023-01-05 14:07:39 +0300

    function recursion projects

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-/**/
+/**
+ *_strlen_recursion - returns string length
+ *@s: string
+ *Return: string length
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (1);
+		return (0);
 	}
 	return (strlen(s));
 }
