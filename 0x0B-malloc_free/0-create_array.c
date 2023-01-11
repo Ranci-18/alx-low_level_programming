cb4085506397ab2b79564ba7a6871c86e9d7b00b
Frank <wanyoike39@gmail.com>
2023-01-11 08:46:48 +0300

    malloc applications and exercises

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *create_array - creates array s
+ *@size: size of array
+ *@c: elements of array
+ *Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(s) * size);
+	if (s == NULL)
+       	{
+               	return (NULL);
+       	}
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+ALX projects concerning malloc function
