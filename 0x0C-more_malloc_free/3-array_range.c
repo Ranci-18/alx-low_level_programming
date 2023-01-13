227d5b93cfa7e6dbdc03afaca3394fea72f0ad63
Frank <wanyoike39@gmail.com>
2023-01-13 21:05:48 +0300

    more malloc funtion projects

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max - min) + 1);
+	arr = malloc(sizeof(*arr) * ((max - min) + 1));
 	if (arr == NULL)
 		return (NULL);
 
