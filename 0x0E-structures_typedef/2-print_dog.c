07a9449400c3d93a619489e4f876f26fc42a7738
Frank <wanyoike39@gmail.com>
2023-01-18 01:13:46 +0300

    C structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 	{
 		if (!(d->name))
 			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
 
-		printf("Age: %.6f\n", d->age);
+		printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
 
 		if (!(d->owner))
 			printf("(nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
+
+		printf("Owner: %s\n", d->owner);
 	}
 }
