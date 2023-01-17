074b2c7e0e2744e0819f57f269d84dd51ef90306
Frank <wanyoike39@gmail.com>
2023-01-17 23:41:17 +0300

    C structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include "main.h"
+/**
+ *init_dog - initializes struct dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
