19b5a5f388d56232a97a13f7bf60c9f011f20395
Frank <wanyoike39@gmail.com>
2023-01-19 17:57:49 +0300

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,10 @@
  *print_name - prints name
  *@name: name to be printed
  *@f: function pointer
- *Return: nothing
+ *Return: nothinig
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f && name)
+		f(name);
 }
