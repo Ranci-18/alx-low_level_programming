b5253dc9ed60e49435d0497d9549b881565c645c
Frank <wanyoike39@gmail.com>
2023-01-22 21:40:41 +0300

    variadic functions project

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 	int sum_of_all = 0;
-        
+
 	if (n == 0)
-                return (0);
+		return (0);
 
 	va_start(numbers, n);
 
