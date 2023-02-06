2ace99813684053f35dc8c9e7313e6acfa5f3c66
Frank <wanyoike39@gmail.com>
2023-02-07 00:18:07 +0300

    bit manipulation tasks

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *binary_to_uint - prints binary representaion of a number 
+ *@n: number 
+ * 
+ * 
+ *Return: no return 
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		putchar((n & 1) + '0');
+	}
+	else
+	{
+		putchar ('0');
+	}
+}
