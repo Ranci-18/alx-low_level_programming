dc304af752b3efb5e64edf16472eb2555c167488
Frank <wanyoike39@gmail.com>
2023-02-07 01:03:28 +0300

    bit manipulation tasks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *get_bit - returns value of a bit at a given index
+ *@n: number
+ *@index: position of bit
+ *Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int mask = 1 << index;
+	int result = n & mask;
+
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	if (n >> 0 && index)
+	{
+		if (result)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+}
