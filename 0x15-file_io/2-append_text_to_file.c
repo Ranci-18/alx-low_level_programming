c874106f37f1fa1c3787c1df550967a9fe3e6c8f
Frank <wanyoike39@gmail.com>
2023-02-08 15:56:59 +0300

    file input/output tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_WRONLY | 0_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
