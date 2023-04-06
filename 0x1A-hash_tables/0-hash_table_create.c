6653ed918d332e9916797ce0d4753dea57719da6
Frank <wanyoike39@gmail.com>
2023-04-06 16:40:20 +0300

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,19 +2,18 @@
 /**/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    int i, size, count;
+    unsigned long int i;
     hash_table_t *h_table = malloc(sizeof(hash_table_t));
     if (!h_table)
     {
         return (NULL);
     }
     h_table->size = size;
-    h_table->count = 0;
-    h_table->items = malloc(sizeof(hash_node_t));
+    h_table->array = malloc(sizeof(hash_node_t));
 
     for (i = 0; i < h_table->size; i++)
     {
-        h_table->items = NULL;
+        h_table->array = NULL;
     }
     return (h_table);
 }
