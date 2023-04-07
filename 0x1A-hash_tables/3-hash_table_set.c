bc2e6c87e018fb0ffd0b53679a7f379e0f2be9d9
Frank <wanyoike39@gmail.com>
2023-04-07 20:15:04 +0300

    fixed task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
- *
- *
+ *hash_table_set - sets a new element to the hash table
+ *@ht: the hashtable
+ *@key: key
+ *@value: value to set
+ *Return: 1 for success, 0 for failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -22,20 +22,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	
 	if (strcmp(pair_el->key, key) == 0)
 	{
-		pair_el->value = (char *)value;
+		if (strcmp(pair_el->value, value) != 0)
+		{
+			free(pair_el->value);
+			pair_el->value = strdup(value);
+		}
 	}
-	else
-	{
-		new_pair_el = malloc(sizeof(hash_node_t));
-		if (new_pair_el == NULL)
-			return (0);
+	return (1);
+	
+	new_pair_el = malloc(sizeof(hash_node_t));
+	if (new_pair_el == NULL)
+		return (0);
 
-		new_pair_el->key = (char *)key;
-		new_pair_el->value = (char *)value;
-		new_pair_el->next = pair_el->next;
-		pair_el->next = new_pair_el;
+	new_pair_el->next = NULL;
+	new_pair_el->key = strdup(key);
+	if (new_pair_el->key == NULL)
+	{
+		free(new_pair_el);
+		return (0);
 	}
-	free(ht);
-	free(new_pair_el);
+	new_pair_el->value = strdup(value);
+	if (new_pair_el-> value == NULL)
+	{
+		free(new_pair_el->key);
+		free(new_pair_el);
+		return (0);
+	}
+	new_pair_el->next = ht->array[index];
+	ht->array[index] = new_pair_el;
+	
 	return (1);
 }
