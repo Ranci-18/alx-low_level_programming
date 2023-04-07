5c2f4dc1fa5a68a7aea93242bf1fdb06c74d8f43
Frank <wanyoike39@gmail.com>
2023-04-07 14:16:43 +0300

    fixed task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 
-	hash_table_t *h_table = malloc(sizeof(hash_table_t));
+	hash_table_t *h_table = calloc(sizeof(hash_table_t), sizeof(hash_node_t));
 
 	if (!h_table)
 	{
