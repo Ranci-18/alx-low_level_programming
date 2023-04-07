3311604c55e3202555435138d5ffe78156ba801d
Frank <wanyoike39@gmail.com>
2023-04-07 14:09:37 +0300

    fixed task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	return (NULL);
 	}
 	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t));
+	h_table->array = calloc(h_table->size, sizeof(hash_node_t));
 
 	for (i = 0; i < h_table->size; i++)
 	{
