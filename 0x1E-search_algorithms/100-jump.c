68de8a7bfb4271fab6651f1e763e7ac0229e20ee
Frank <wanyoike39@gmail.com>
2023-07-12 15:11:32 +0300

    advanced task 1 and 2

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+/**
+ *linear_search - searches for a number sequentially
+ *@array: array pointer
+ *@prev: initial index
+ *@size: size of array
+ *
+ *Return: index of number
+ */
+int linear_srch(int *array, int prev, size_t size, int value)
+{
+	int i = 0;
+
+	printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	for (i = prev; i < (int)size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", prev, i);
+			return (i);
+		}
+	}
+	return (-1);
+}
+
+/**
+ *jump_search - searches for value in a sorted array
+ *@array: the array to search in
+ *@size: size of array
+ *@value: number to search for
+ *
+ *
+ *Return: index of value or -1 if value is not present
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump = (int)sqrt(size);
+	int prev = 0;
+	int next;
+
+	if (array == NULL)
+		return (-1);
+
+	while (array[prev] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		next = prev + jump;
+		if (next >= (int)size)
+			next = (int)size - 1;
+
+		if (array[next] >= value)
+			break;
+
+		prev += jump;
+
+		if (prev >= (int)size)
+			return (-1);
+	}
+	return (linear_srch(array, prev, size, value));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,8 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
+int linear_srch(int *array, int prev, size_t size, int value);
+
 #endif
